e53ee66f5402129de4e7465e4c11c0a01b39e82b
Baturax <bai@baturax.com>
2025-07-14 02:38:45 +0300

    ..

---
+++ src/main/kotlin/org/dokiteam/doki/parsers/site/mangareader/tr/Gafeland.kt
@@ -0,0 +1,17 @@
+package org.dokiteam.doki.parsers.site.mangareader.tr
+
+import org.dokiteam.doki.parsers.MangaLoaderContext
+import org.dokiteam.doki.parsers.MangaSourceParser
+import org.dokiteam.doki.parsers.model.MangaListFilterCapabilities
+import org.dokiteam.doki.parsers.model.MangaParserSource
+import org.dokiteam.doki.parsers.site.mangareader.MangaReaderParser
+
+@MangaSourceParser("GAFELAND", "Gafeland", "tr")
+internal class Gafeland(context: MangaLoaderContext) :
+	MangaReaderParser(context, MangaParserSource.GAFELAND, "www.gafeland.com", pageSize = 20, searchPageSize = 10) {
+	override val filterCapabilities: MangaListFilterCapabilities
+		get() = super.filterCapabilities.copy(
+			isTagsExclusionSupported = false,
+		)
+}
+
